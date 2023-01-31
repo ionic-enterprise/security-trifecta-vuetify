ab1ba143658342f578b99d7f42a66c892bf94055
Ken Sodemann <ken@ionic.io>
2023-01-31 08:35:46 -0600

    feat(notes): create the note editor

--- src/tasting-notes/TastingNotesPage.vue
+++ src/tasting-notes/TastingNotesPage.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="pa-4">
+    <!-- <TastingNoteEditor class="mb-4" v-for="note in notes" :key="note.data.id" :note="note" /> -->
     <TastingNoteViewer class="mb-4" v-for="note in notes" :key="note.data.id" :note="note" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useTastingNotes } from '@/tasting-notes/composables/tasting-notes';
+import TastingNoteEditor from '@/tasting-notes/components/TastingNoteEditor.vue';
 import TastingNoteViewer from '@/tasting-notes/components/TastingNoteViewer.vue';
 
 const { notes, refresh } = useTastingNotes();
