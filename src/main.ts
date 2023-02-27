25f091b0b072c2dac69e71466f16bf3c80294eef
Ken Sodemann <ken@ionic.io>
2023-02-27 13:32:24 -0600

    feat(notes): hook up the editor

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { registerPlugins } from '@/plugins';
 import { Device } from '@ionic-enterprise/identity-vault';
 import { createApp } from 'vue';
 import App from './App.vue';
+import './styles/Button.css';
 import './styles/responsive.css';
 
 Device.setHideScreenOnBackground(true);
